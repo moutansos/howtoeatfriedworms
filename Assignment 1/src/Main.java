ab033d379d372abfac65930923e8114b6e61a6f5
ben <moutansos@gmail.com>
2018-09-21 10:44:04 -0700

    writeToFile function

--- Assignment 1/src/Main.java
+++ Assignment 1/src/Main.java
@@ -1,6 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+    private const int iteration = 0;
+    private static String program = "import java.io.BufferedWriter;\n" +
+            "import java.io.FileWriter;\n" +
+            "import java.io.IOException;\n" +
+            "\n" +
+            "public class Main {\n" +
+            "    private const int iteration = " + (iteration + 1) + ";\n" +
+            "    private static String program = \"" + program + "\";\n" +
+            "\n" +
+            "    public static void main(String[] args) {\n" +
+            "        writeStringToFile(program);\n" +
+            "    }\n" +
+            "\n" +
+            "    public static void writeStringToFile(String input) throws IOException {\n" +
+            "        BufferedWriter writer = new BufferedWriter(new FileWriter(\"Iteration-\" + iteration + \".java\"));\n" +
+            "        writer.write(input);\n" +
+            "        writer.close();();\n" +
+            "    }\n" +
+            "}\n";
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            writeStringToFile(program);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void writeStringToFile(String input) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Iteration-" + iteration + ".java"));
+        writer.write(input);
+        writer.close();();
     }
 }
