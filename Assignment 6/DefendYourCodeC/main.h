8ad9d31aa7cabf9cd539f3d75361c0affde26392
traviscurrier <39418730+traviscurrier@users.noreply.github.com>
2018-11-04 15:23:15 -0800

    included hash fucntion

--- Assignment 6/DefendYourCodeC/main.h
+++ Assignment 6/DefendYourCodeC/main.h
@@ -3,5 +3,6 @@ long promptForValidInteger(const char promptText[]);
 void promptForValidFileName(char buff[], const int bufferSize, const char promptFile[], const char regexFile[], const bool fileMustAlreadyExist);
 void storePassword(char * firstPassword);
 void comparePasswords(char * secondPassword);
-void outputInfoToFile(char * firstName, char * lastName, int val1, int val2);
+void outputInfoToFile(char * firstName, char * lastName, int val1, int val2, char * readFileName, char * writeFileName);
 void clearBuffer(char *input);
+long hash(char * password);
